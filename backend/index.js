acdbac3b4a28fabe10cebf0658e83ed54837c0dd
nuaimhasan <nuaim.emanager@gmail.com>
2024-10-30 13:33:48 +0600

    update backend

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(
     origin: [
       "https://software.emanagerit.com",
       "https://www.software.emanagerit.com",
+      "http://localhost:3050",
     ],
     credentials: true,
   })
